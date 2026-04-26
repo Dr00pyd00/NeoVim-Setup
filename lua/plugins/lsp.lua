51f0a0e03aedf3b2c680e88aac2d504aa38b29df
Luna <kottakcode@gmail.com>
2026-04-26 13:47:11 +0300

    feat: init commit

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,20 @@
+vim.lsp.enable("pyright")
+
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "pyright" },
+      })
+    end,
+  },
+}
+
