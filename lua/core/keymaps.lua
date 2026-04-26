87726178fb5326530abf2b80b80671256718e232
Luna <kottakcode@gmail.com>
2026-04-26 21:45:18 +0300

    fix shortcut terminal

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,3 +1,9 @@
 -- Diagnostics
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Afficher l'erreur" })
+vim.keymap.set("n", "<leader>ep", vim.diagnostic.open_float, { desc = "Erreur Python" })
+
+-- Terminal
+vim.keymap.set("n", "<leader>t", function()
+  vim.cmd("botright split")
+  vim.cmd("terminal")
+end, { desc = "Terminal" })
 
